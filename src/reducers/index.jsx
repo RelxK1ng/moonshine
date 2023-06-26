723ee1406aa4009dacb8a9035a60eafa8ca0b75c
Jiang <54928439+jchhh912@users.noreply.github.com>
2023-06-26 15:10:42 +0800

    Delete src directory

--- src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import authReducer from './auth'
-import noteReducer from './notes'
-
-export default combineReducers({
-  auth: authReducer,
-  notes: noteReducer
-})
